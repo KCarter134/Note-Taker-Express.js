e730fd80bd153c8e3bfd67f96c4cef95a3de745a
Kyle Carter <kcarter134@yahoo.com>
2022-08-15 19:03:27 -0400

    added proper routing and express.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+
+// allows for use of express app
+const app = express();
+// Sets port for listening and let heroku decide on port, if not, use port 8080
+const PORT = process.env.PORT || 3002;
+
+//serve images, CSS files, and JavaScript files in a directory named public
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
