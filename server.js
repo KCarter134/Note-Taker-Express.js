852c6f414ccff874618d0d7133c542d44b656f6b
Kyle Carter <kcarter134@yahoo.com>
2022-08-17 17:51:23 -0400

    update code

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require("path");
 
 // allows for use of express app
 const app = express();
-// Sets port for listening and let heroku decide on port, if not, use port 8080
+// Sets port for listening and serving
 const PORT = process.env.PORT || 3002;
 
 //serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
