1739579d280482cab453962968df5ebc1c8d1957
Kyle Carter <kcarter134@yahoo.com>
2022-08-18 12:56:25 -0400

    working page

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-const fs = require("fs");
 const express = require("express");
-const path = require("path");
-
-// allows for use of express app
 const app = express();
-// Sets port for listening and serving
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3004;
+const path = require("path");
+const fs = require("fs");
 
-//serve images, CSS files, and JavaScript files in a directory named public
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+//route to index page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
 
+//route to notes
+app.get("/public/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
+
+//return all JSON input
+app.get("/public/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "Develop/db/db.json"));
+});
+
+
+app.get("/notes", (req, res) => res.sendFile(__dirname + "/Develop/public/notes.html"));
+// return index.html after written
+app.get("*", (req, res) => res.sendFile(__dirname + "/Develop/public/index.html"));
+
+//listen tot he port when deployed
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
